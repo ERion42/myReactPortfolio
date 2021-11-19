d175208577f7223fded9d43147e60d223e6bc17b
Eric Rion <erion42@gmail.com>
2021-11-18 22:51:34 -0500

    Styling for Home almost done, still needs words added. About Section needs styling. Portfolio needs styling, but otherwise done. Contact/Resume need work

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -18,10 +18,9 @@ export default function Landing() {
 
       <div class="jumbotron">
       <div class="container">
-        <img id= "skyline" src={skyline} class="w-100" alt="Stock, to be replaced." />
+        <img id= "skyline" src={skyline} class="w-100 pt-3 pb-5" alt="Stock, to be replaced." />
         <h1 class="display-3">Hello, world!</h1>
-        <p>Must go faster... go, go, go, go, go! So you two dig up, dig up dinosaurs? So you two dig up, dig up dinosaurs? Jaguar shark! So tell me - does it really exist? Must go faster... go, go, go, go, go! Just my luck, no ice. I was part of something special.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+        <p class="pb-3">Must go faster... go, go, go, go, go! So you two dig up, dig up dinosaurs? So you two dig up, dig up dinosaurs? Jaguar shark! So tell me - does it really exist? Must go faster... go, go, go, go, go! Just my luck, no ice. I was part of something special.</p>
       </div>
     </div>
     <div class="container">
@@ -30,17 +29,14 @@ export default function Landing() {
       <div class="col-md-4">
         <h2>Heading</h2>
         <p>Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! So you two dig up, dig up dinosaurs? You really think you can fly that thing? Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
       <div class="col-md-4">
         <h2>Heading</h2>
         <p>Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Life finds a way. What do they got in there? King Kong? Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists. God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
       <div class="col-md-4">
         <h2>Heading</h2>
         <p>Life finds a way. They're using our own satellites against us. And the clock is ticking. Yes, Yes, without the oops! You really think you can fly that thing? Must go faster. Checkmate... What do they got in there? King Kong? Is this my espresso machine? Wh-what is-h-how did you get my espresso machine?</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
     </div>
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -8,59 +8,59 @@ import WorkoutTracker from "./img/WorkoutTracker.png";
 
 export default function Portfolio() {
   return (
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img id= "budgetTracker" src={BudgetTracker} class="d-block mx-auto" alt="..."></img>
-                <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
-                    <h2>Budget Tracker</h2>
-                    <p>HTML, CSS, JavaScript, MongoDB</p>
-                </div>                
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" >
+            <div class="carousel-indicators mb-3">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
             </div>
-            <div class="carousel-item">
-                <img id= "WorkoutTracker" src={WorkoutTracker} class="d-block mx-auto" alt="..."></img>
-                <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
-                    <h2>Workout Tracker</h2>
-                    <p>HTML, CSS, Javascript, MongoDB</p>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img id= "budgetTracker" src={BudgetTracker} class="d-block mx-auto" alt="..." style={{ height: "50%" }}></img>
+                    <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
+                        <h2>Budget Tracker</h2>
+                        <p>HTML, CSS, JavaScript, MongoDB</p>
+                    </div>                
+                </div>
+                <div class="carousel-item">
+                    <img id= "WorkoutTracker" src={WorkoutTracker} class="d-block mx-auto" alt="..."></img>
+                    <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
+                        <h2>Workout Tracker</h2>
+                        <p>HTML, CSS, Javascript, MongoDB</p>
+                    </div>        
+                </div>
+                <div class="carousel-item">
+                    <img id= "DateNightPicker" src={DateNightPicker} class="d-block mx-auto" alt="..."></img>
+                    <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
+                        <h2>Project: Date Night</h2>
+                        <p>HTML, CSS, JavaScript, Bootstrap, Server-Side APIs</p>
+                    </div>        
+                </div>
+                <div class="carousel-item">
+                    <img id= "Scheduler" src={Scheduler} class="d-block mx-auto" alt="..."></img>
+                    <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
+                        <h2>Scheduler</h2>
+                        <p>HTML, CSS, JavaScript, Local Storage</p>
+                    </div>        
+                </div>            
+                <div class="carousel-item">
+                    <img id= "EventFinder" src={EventFinder} class="d-block mx-auto" alt="..."></img>
+                    <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
+                        <h2>Event Finder</h2>
+                        <p>HTML, CSS, JavaScript, APIs</p>
+                    </div>        
                 </div>        
             </div>
-            <div class="carousel-item">
-                <img id= "DateNightPicker" src={DateNightPicker} class="d-block mx-auto" alt="..."></img>
-                <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
-                    <h2>Project: Date Night</h2>
-                    <p>HTML, CSS, JavaScript, Bootstrap, Server-Side APIs</p>
-                </div>        
-            </div>
-            <div class="carousel-item">
-                <img id= "Scheduler" src={Scheduler} class="d-block mx-auto" alt="..."></img>
-                <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
-                    <h2>Scheduler</h2>
-                    <p>HTML, CSS, JavaScript, Local Storage</p>
-                </div>        
-            </div>            
-            <div class="carousel-item">
-                <img id= "EventFinder" src={EventFinder} class="d-block mx-auto" alt="..."></img>
-                <div class="carousel-caption d-none d-md-block text-light bg-primary rounded-3">
-                    <h2>Event Finder</h2>
-                    <p></p>
-                </div>        
-            </div>        
-        </div>
-        <button class="carousel-control-prev text-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="false"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="false"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+            <button class="carousel-control-prev text-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="false"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
   );
 }
