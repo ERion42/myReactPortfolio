9cb833df50581f6b3071d405a3eeec52351346db
Eric Rion <erion42@gmail.com>
2021-11-19 18:24:52 -0500

    Landing page is finished.

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -19,24 +19,24 @@ export default function Landing() {
       <div class="jumbotron">
       <div class="container">
         <img id= "skyline" src={skyline} class="w-100 pt-3 pb-5" alt="Stock, to be replaced." />
-        <h1 class="display-3">Hello, world!</h1>
-        <p class="pb-3">Must go faster... go, go, go, go, go! So you two dig up, dig up dinosaurs? So you two dig up, dig up dinosaurs? Jaguar shark! So tell me - does it really exist? Must go faster... go, go, go, go, go! Just my luck, no ice. I was part of something special.</p>
+        <h1 class="display-3">Web Design and Innovation!</h1>
+        <p class="pb-3">If you want a front-end developer with years of experience both working with and leading individuals in a variety of fields and environments, I'm your guy.</p>
       </div>
     </div>
     <div class="container">
     {/* <!-- Example row of columns --> */}
     <div class="row">
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! So you two dig up, dig up dinosaurs? You really think you can fly that thing? Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists.</p>
+        <h2>Communication</h2>
+        <p>For the past decade I've been both working with and leading teams of capable individuals in jobs ranging from retail to the banking industry to technical and customer support. I can either lead or follow in order to create the best team synergy possible.</p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Life finds a way. What do they got in there? King Kong? Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists. God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.</p>
+        <h2>Adaptive</h2>
+        <p>The wide range of experiences I've had have given me a considerable array of skills. From customer service to technical support, I adapt quickly and excel at any work task I'm given. Staying flexible helps me work in different types of groups and settings and keeps things exciting.</p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Life finds a way. They're using our own satellites against us. And the clock is ticking. Yes, Yes, without the oops! You really think you can fly that thing? Must go faster. Checkmate... What do they got in there? King Kong? Is this my espresso machine? Wh-what is-h-how did you get my espresso machine?</p>
+        <h2>Web Development</h2>
+        <p>Most of my experience has been in dealing with data servers by way of Microsoft Access or SQL Server. Lately I've fully immersed myself in a Full Stack Boot Camp course at Georgia Tech. In addition to skills ranging from basic HTML, CSS, and JavaScript, I've also learned to work in teams to build projects as well as how to handle the pressure of coding deadlines.</p>
       </div>
     </div>
 
