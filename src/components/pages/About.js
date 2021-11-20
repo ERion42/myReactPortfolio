0381a281a33ec16730e0477d3ea54f7672815c32
Eric Rion <erion42@gmail.com>
2021-11-19 21:01:47 -0500

    Minor change to alignment of About section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,7 +19,7 @@ export default function About() {
         </div> */}
 
         <section class="container">
-          <div class="row align-items-center">
+          <div class="row align-items-top">
             <div class="col" >
                   <p>My name is Eric Rion and I'm a web developer that lives in the suburbs outside of Atlanta, Ga. with my wife and our cat, Lenore.</p>
 
