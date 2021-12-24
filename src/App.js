746ab4d88afe8dc2aed10782c17ddb2b18975b18
Eric Rion <erion42@gmail.com>
2021-12-23 23:08:11 -0500

    Updates

--- src/App.js
+++
@@ -1,7 +0,0 @@
-// Updated for new project
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-
-const App = () => <PortfolioContainer />;
-
-export default App;
